5264d01a6a7292d017d3141dae65607045835ba5
GabrielOt1en0 <129309310+GabrielOt1en0@users.noreply.github.com>
2026-07-22 11:32:22 +0300

    Update route components for login and signup

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,8 @@ export default function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
         <Route
           path="/dashboard"
           element={
