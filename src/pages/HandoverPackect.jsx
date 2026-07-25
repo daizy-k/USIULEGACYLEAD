1a256c55f59536a56eb80d4498382c10175195f1
nyeliep <gielpalnyeliep@gmail.com>
2026-07-25 11:50:35 +0300

    Add admin dashboard: review/approve flow, org registration, deadlines, reports

--- src/pages/HandoverPackect.jsx
+++
@@ -1,8 +0,0 @@
-export default function HandoverPackets() {
-  return (
-    <div>
-      <h1>Handover Packets</h1>
-      {/* Cloudinary document upload + Firestore list goes here */}
-    </div>
-  );
-}
